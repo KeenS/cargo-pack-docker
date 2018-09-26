1a4bdb7b2ce4ece99ac3f64d965eac0083f6d0da
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2018-09-26 21:34:49 +0900

    do not run

--- ci/script.sh
+++ ci/script.sh
@@ -9,9 +9,8 @@ main() {
         return
     fi
 
-    cross test --target $TARGET
+    cross test --target $TARGET --release
 
-    cross run --target $TARGET --release
 }
 
 # we don't run the "test phase" when doing deploys
